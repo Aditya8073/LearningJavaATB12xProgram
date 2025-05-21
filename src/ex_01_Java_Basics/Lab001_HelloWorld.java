cfa47bd6c985ae3502523b12d71b4684064dbfb0
Aditya8073 <adityaverma06031998@gmail.com>
2025-05-21 23:30:13 +0530

    added Lab001_HelloWorld

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        int a=2;
+        int b=5;
+        int sum=a+b;
+        System.out.println("The sum of a & b is: " +sum);
+    }
+}
