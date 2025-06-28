a2a6507227b9f6f45c1f3c694b6036dd9d025bf8
Aditya8073 <adityaverma06031998@gmail.com>
2025-06-28 14:41:03 +0530

    28th June 2025

---
+++ src/StringCodingAssessment/BasicStringOperations_Challenge1.java
@@ -0,0 +1,12 @@
+package StringCodingAssessment;
+
+public class BasicStringOperations_Challenge1 {
+    public static void main(String[] args) {
+        String s= "Hello";
+        s=s+" World";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+        System.out.println(s.substring(6));
+    }
+}
