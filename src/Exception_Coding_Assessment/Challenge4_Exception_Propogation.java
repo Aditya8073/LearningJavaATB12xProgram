ddaa580fdaae3e81d315c87e8f3afea9a77a33e7
Aditya8073 <adityaverma06031998@gmail.com>
2025-07-05 13:12:18 +0530

    Update and rename Challenge3_Exception_Propogation.java to Challenge4_Exception_Propogation.java

--- src/Exception_Coding_Assessment/Challenge4_Exception_Propogation.java
+++ src/Exception_Coding_Assessment/Challenge4_Exception_Propogation.java
@@ -1,6 +1,6 @@
 package Exception_Coding_Assessment;
 
-public class Challenge3_Exception_Propogation {
+public class Challenge4_Exception_Propogation {
     //Show how an exception propagates through method calls.
     //InputChain of method calls with division by zero
     //Output=Exception caught in main method
