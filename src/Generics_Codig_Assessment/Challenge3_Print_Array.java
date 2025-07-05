67141b883b66e4c82c35b2bb4687ee6efda8d6cf
Aditya8073 <adityaverma06031998@gmail.com>
2025-07-05 13:14:06 +0530

    Update and rename Challenge_Print_Array.java to Challenge3_Print_Array.java

--- src/Generics_Codig_Assessment/Challenge3_Print_Array.java
+++ src/Generics_Codig_Assessment/Challenge3_Print_Array.java
@@ -1,6 +1,6 @@
 package Generics_Codig_Assessment;
 
-public class Challenge_Print_Array {
+public class Challenge3_Print_Array {
 //    Write a generic method to print any type of array.
     //Input=String array ["A", "B", "C"]
     //Output=A B C
