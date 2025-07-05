c04a05aea0ecaf3af850de05b8d70ef895b3d979
Aditya8073 <adityaverma06031998@gmail.com>
2025-07-05 23:32:27 +0530

    5th July 2025

--- src/Exception_Coding_Assessment/Challenge3_DifferentTypeExceptions.java
+++ src/Exception_Coding_Assessment/Challenge3_DifferentTypeExceptions.java
@@ -1,6 +1,6 @@
 package Exception_Coding_Assessment;
 
-public class Challenege3_DifferentTypeExceptions {
+public class Challenge3_DifferentTypeExceptions {
     //Write a program that catches both `ArithmeticException` and `ArrayIndexOutOfBoundsException`.
     //Input=Division by zero in array operation
     //Output=Exception caught: ArithmeticException or ArrayIndexOutOfBoundsException
